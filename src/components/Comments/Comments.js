ad3f089830236f8428339a0b421b428eed155f01
tarmes <traarmes@gmail.com>
2020-09-08 18:58:26 -0400

    MVP

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -11,7 +11,7 @@ const Comments = props => {
       {/* map through the comments prop and render a Comment for every piece of data */}
       {
         comments.map(commentObj => {
-          return <Comment comment={commentObj.text} />
+          return <Comment comment={commentObj} />
         })
       }
     </div>
